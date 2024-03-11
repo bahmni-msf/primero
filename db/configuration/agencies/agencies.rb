197e8a14aa1a3901852983461b23133d8ece9f9a
saisamyukthag <saisamyuktha.g@thoughtworks.com>
2024-03-11 17:44:25 +0530

    POTM-31 | [Samyuktha | Sreelekha] | Create new agency MSF

--- db/configuration/agencies/agencies.rb
+++ db/configuration/agencies/agencies.rb
@@ -2,17 +2,32 @@
 
 # Copyright (c) 2014 - 2023 UNICEF. All rights reserved.
 
-unicef = Agency.create_or_update!(
-  name_en: 'UNICEF',
-  agency_code: 'UNICEF',
-  unique_id: 'UNICEF'
+# unicef = Agency.create_or_update!(
+#   name_en: 'UNICEF',
+#   agency_code: 'UNICEF',
+#   unique_id: 'UNICEF'
+# )
+#
+# unicef.logo_full.attach(
+#   io: File.open("#{File.dirname(__FILE__)}/unicef-full.png"), filename: 'unicef-full.png'
+# )
+# unicef.logo_icon.attach(
+#   io: File.open("#{File.dirname(__FILE__)}/unicef-icon.png"), filename: 'unicef-icon.png'
+# )
+# unicef.logo_enabled = true
+# unicef.save!
+
+msf = Agency.create_or_update!(
+  name_en: 'MSF',
+  agency_code: 'MSF',
+  unique_id: 'MSF'
 )
 
-unicef.logo_full.attach(
-  io: File.open("#{File.dirname(__FILE__)}/unicef-full.png"), filename: 'unicef-full.png'
+msf.logo_full.attach(
+  io: File.open("#{File.dirname(__FILE__)}/msf-full.png"), filename: 'msf-full.png'
 )
-unicef.logo_icon.attach(
-  io: File.open("#{File.dirname(__FILE__)}/unicef-icon.png"), filename: 'unicef-icon.png'
+msf.logo_icon.attach(
+  io: File.open("#{File.dirname(__FILE__)}/msf-full.png"), filename: 'msf-full.png'
 )
-unicef.logo_enabled = true
-unicef.save!
+msf.logo_enabled = true
+msf.save!
