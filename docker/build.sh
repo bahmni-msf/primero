c9d4d2c880c7ad435b0a90481107e4309d96e98f
akshaypawartw <akshay.pawar@thoughtworks.com>
2024-03-20 18:58:01 +0530

    pom 42 | build.sh fix 2

--- docker/build.sh
+++ docker/build.sh
@@ -9,7 +9,7 @@ set -euxo pipefail
 source ./docker/defaults.env
 test -e ./local.env && source ./local.env
 
-USAGE="Usage ./build application|migration|nginx|solr|all [-t <tag>] [-r <repository>] [-b <registry>] [-l]"
+USAGE="Usage ./build application|migration|nginx|solr|all [-t <tag>] [-r <repository>] [-b <registry>] [-d <docker_registry>] [-l]"
 
 if [[ $# -eq 0 ]]; then
   echo "${USAGE}"
@@ -19,7 +19,7 @@ fi
 image=${1}
 shift || true
 
-while getopts "t:r:b:l" opt ; do
+while getopts "t:r:b:d:l" opt ; do
   case ${opt} in
     t )
       t=$OPTARG
@@ -29,6 +29,9 @@ while getopts "t:r:b:l" opt ; do
     ;;
      b )
       b="${OPTARG}/"
+    ;;
+     d )
+      d="${OPTARG}/"
     ;;
     l )
       l=true
@@ -44,7 +47,8 @@ shift $((OPTIND -1)) || true
 tag=${t:-latest}
 repository=${r:-""}
 with_latest=${l:-false}
-build_registry=${b:-"uniprimeroxacrdev.azurecr.io"}
+build_registry=${b:-""}
+docker_registry=${d:-""}
 
 BUILD_NGINX="docker build -f ./docker/nginx/Dockerfile ./docker -t ${repository}:${tag} --build-arg NGINX_UID=${NGINX_UID} --build-arg NGINX_GID=${NGINX_GID} --build-arg BUILD_REGISTRY=${build_registry}"
 BUILD_SOLR="docker build -f ./docker/solr/Dockerfile . -t ${repository}:${tag} --build-arg BUILD_REGISTRY=${build_registry}"
@@ -55,31 +59,22 @@ apply_tags_and_push () {
   local subtag=${2:-""}
   [[ -n "${subtag}" ]] && subtag="-${subtag}"
 
-  docker tag "${repository}:${tag}${subtag}" "${build_registry}/${repository}:${tag}${subtag}" > /dev/null
+  docker tag "${repository}:${tag}${subtag}" "${docker_registry}/${repository}:${tag}${subtag}" > /dev/null
   if [[ "${with_latest}" == true ]] ; then
     docker tag "${repository}:${tag}${subtag}" "${repository}:latest${subtag}" > /dev/null
-    docker tag "${repository}:${tag}${subtag}" "${build_registry}/${repository}:latest${subtag}" > /dev/null
+    docker tag "${repository}:${tag}${subtag}" "${docker_registry}/${repository}:latest${subtag}" > /dev/null
   fi
   # Push the tagged images to the repository
-  docker push "${build_registry}/${repository}:${tag}${subtag}" > /dev/null
+  docker push "${docker_registry}/${repository}:${tag}${subtag}" > /dev/null
   if [[ "${with_latest}" == true ]] ; then
-    docker push "${build_registry}/${repository}:latest${subtag}" > /dev/null
+    docker push "${docker_registry}/${repository}:latest${subtag}" > /dev/null
   fi
   # Output the image name
-  echo "${build_registry}/${repository}:${tag}${subtag}"
+  echo "${docker_registry}/${repository}:${tag}${subtag}"
   if [[ "${with_latest}" == true ]] ; then
-    echo "${build_registry}/${repository}:latest${subtag}"
+    echo "${docker_registry}/${repository}:latest${subtag}"
   fi
 }
-
-# Parse image names and corresponding ECR repositories
-# IFS=',' read -r -a images <<< "${IMAGE_NAMES}"
-# IFS=',' read -r -a ecr_repos <<< "${ECR_REPO}"
-
-# Loop through each image and its corresponding repository
-# for (( i=0; i<${#images[@]}; i++ )); do
-#   image_name="${images[i]}"
-#   ecr_repo="${ecr_repos[i]}"
 
   case ${image} in
     nginx)
@@ -102,4 +97,3 @@ apply_tags_and_push () {
       exit 1
     ;;
   esac
-# done
