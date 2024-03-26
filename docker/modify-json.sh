0f15e4506a5b6c877e731fa7ea473715f74c0b60
akshaypawartw <akshay.pawar@thoughtworks.com>
2024-03-26 17:36:34 +0530

    pom 42 | construct a json object 2

--- docker/modify-json.sh
+++ docker/modify-json.sh
@@ -1,43 +1,40 @@
 set -euxo pipefail
 
-          # Read image names from environment variables
-          echo "Generated Image Names=$IMAGE_NAMES"
+# Read image names from environment variables
+echo "Generated Image Names=$IMAGE_NAMES"
 
-          # Update the image field in the services JSON with the generated image names
-          IMAGE_NAMES=$(echo "$IMAGE_NAMES" | tr -d '\n')
+# Update the image field in the services JSON with the generated image names
+IMAGE_NAMES=$(echo "$IMAGE_NAMES" | tr -d '\n')
 
-          # Check if image names are empty
-          if [ -z "$IMAGE_NAMES" ]; then
-            echo "Error: No image names were generated."
-            exit 1
-          fi
+# Check if image names are empty
+if [ -z "$IMAGE_NAMES" ]; then
+  echo "Error: No image names were generated."
+  exit 1
+fi
 
-          # Check if services.json exists
-          if [ ! -f "services.json" ]; then
-            echo "Error: services.json not found."
-            exit 1
-          fi
+# Check if services.json exists
+if [ ! -f "services.json" ]; then
+  echo "Error: services.json not found."
+  exit 1
+fi
 
-          # Construct JSON object for image names
-          IMAGE_JSON="{\"image\": \"$IMAGE_NAMES\"}"
+# Parse the JSON file and capture any errors
+updated_json=$(jq '.' services.json) || { echo "Error: Failed to parse JSON in services.json"; exit 1; }
 
-          # Use jq to merge the constructed JSON with the existing JSON
-          updated_json=$(jq --argjson image_json "$IMAGE_JSON" '.[] |= . + $image_json' services.json)
+# Construct JSON object for image names
+IMAGE_JSON="{\"image\": \"$IMAGE_NAMES\"}"
 
-          # Check if jq command succeeded
-          if [ $? -ne 0 ]; then
-            echo "Error: Failed to update image names in services.json."
-            exit 1
-          fi
+# Use jq to merge the constructed JSON with the existing JSON
+updated_json=$(jq --argjson image_json "$IMAGE_JSON" '.[] |= . + $image_json' <<< "$updated_json") || { echo "Error: Failed to update image names in services.json."; exit 1; }
 
-          # Save the updated JSON to a file
-          if ! echo "$updated_json" > updated_services.json; then
-            echo "Error: Failed to save updated JSON to updated_services.json."
-            exit 1
-          fi
+# Save the updated JSON to a file
+if ! echo "$updated_json" > updated_services.json; then
+  echo "Error: Failed to save updated JSON to updated_services.json."
+  exit 1
+fi
 
-          # Display the updated JSON
-          cat updated_services.json
+# Display the updated JSON
+cat updated_services.json
 
-          # Set the output
-          echo "::set-output name=services-json::$(<updated_services.json)"
+# Set the output
+echo "::set-output name=services-json::$(<updated_services.json)"
