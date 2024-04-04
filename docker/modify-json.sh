afe8c297e0b494e826ceb18be67478dd11a5191a
akshaypawartw <akshay.pawar@thoughtworks.com>
2024-04-04 14:59:06 +0530

    pom 42 | modify json script modified 21

--- docker/modify-json.sh
+++ docker/modify-json.sh
@@ -28,12 +28,9 @@ fi
 # Escape special characters in the image name for sed
 ESCAPED_IMAGE_NAMES=$(echo "$IMAGE_NAMES" | sed 's/[\/&]/\\&/g')
 
-# Filter the JSON array to extract only the object for the specified service
+# Use sed to update the image field for the specified service
+sed -i "/\"service_name\": \"$SERVICE_NAME\"/ {N;s|\"image\": \"\"|\"image\": \"$ESCAPED_IMAGE_NAMES\"|}" "$SERVICES_JSON_PATH"
+
+# Output only the updated JSON object for the specified service
 UPDATED_JSON=$(jq --arg service "$SERVICE_NAME" '.[] | select(.service_name == $service)' "$SERVICES_JSON_PATH")
-
-# Use sed to filter the JSON array and update the image field for the specified service
-# sed -i "/\"service_name\": \"$SERVICE_NAME\"/ {N;s|\"image\": \"\"|\"image\": \"$IMAGE_NAMES\"|}" "$UPDATED_JSON"
-sed -i "/\"service_name\": \"$SERVICE_NAME\"/ {N;s|\"image\": \"\"|\"image\": \"$ESCAPED_IMAGE_NAMES\"|}" "$UPDATED_JSON"
-
-# Output the updated JSON content as a string
 echo "$UPDATED_JSON"
